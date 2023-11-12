d2007a059d88a32b25fc24eed31da8dcf5ef87f2
Leong Chi Wei <chiwei0708@gmail.com>
2023-11-12 16:18:52 +0800

    added genre page for tv shows

--- src/app/tv-shows/genres/[genre]/page.tsx
+++ src/app/tv-shows/genres/[genre]/page.tsx
@@ -1,5 +1,13 @@
-import { TVGenrePageData } from "@/types/tvGenrePageData";
+import type { TVGenrePageData } from "@/types/tvGenrePageData";
+
+import Link from "next/link";
+
+import CardsGrid from "@/components/CardsGrid/CardsGrid";
+import Card from "@/components/Card/Card";
+import Pagination from "@/components/Pagination/Pagination";
+
 import { fetchData } from "@/utils/fetchData";
+import { mockTVGenresPageData } from "@/mocks/mockTVShowGenrePageData";
 
 type GenreProps = {
   params: { genre: string };
@@ -25,6 +33,49 @@ export default async function TVShowsGenrePage({
   searchParams,
 }: GenreProps) {
   const page = parseInt(searchParams.page ?? 1);
+  // const res = await getTVShowGenrePageData(params.genre, page);
+  const res = mockTVGenresPageData;
 
-  return <div>genre for tv shows</div>;
+  return (
+    <div className="md:pt-8 lg:pt-12">
+      <div className="flex items-end justify-between gap-x-2 pt-4 sm:flex-col sm:items-start sm:justify-normal sm:gap-x-0 sm:gap-y-2 sm:pt-6 md:gap-y-2">
+        <h2 className="text-[1.25rem] font-medium capitalize text-[#877887] md:text-[1.563rem] lg:text-[1.953rem]">
+          {params.genre}
+        </h2>
+
+        <div>
+          <Link href="/" className="text-[0.8rem] text-[#F50057]">
+            Back to home
+          </Link>
+        </div>
+      </div>
+
+      <div className="pt-6 md:pt-10">
+        <CardsGrid lesserCols>
+          {res.results.map((tv) => (
+            <Card
+              key={tv.id}
+              id={tv.id}
+              type="tv-show"
+              imagePriority
+              imageUrl={tv.poster_path}
+              name={tv.name}
+              bottomRowProps={{
+                rating: tv.vote_average,
+                releaseDate: tv.first_air_date,
+              }}
+            />
+          ))}
+        </CardsGrid>
+      </div>
+
+      <div className="flex justify-end pt-20 lg:pt-32">
+        <Pagination
+          baseUrl={`/tv-shows/genres/${params.genre}`}
+          currentPage={page}
+          totalPages={res.total_pages}
+        />
+      </div>
+    </div>
+  );
 }
