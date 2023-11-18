dacf0372065f687c20b22c5d85ce89856cf8948b
Leong Chi Wei <chiwei0708@gmail.com>
2023-11-18 20:52:43 +0800

    modified error to show modal

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,27 +1,30 @@
 "use client";
 
-export default function ErrorPage({ reset }: { reset: () => void }) {
+import Link from "next/link";
+
+export default function ErrorPage() {
   return (
     <div className="min-h-[75vh]">
-      <div className="pt-12">
-        <div className="rounded bg-[#1B181B] p-8 lg:p-10">
-          <div className="text-[1.56rem] font-semibold uppercase text-[#CFC9CF] sm:text-[2.44rem]">
-            an error occurred
-          </div>
+      <div className="fixed bottom-0 left-0 right-0 top-0 z-[100] bg-[#121012]/90">
+        <div className="px-6 pt-20 sm:px-14 md:px-[4.5rem] md:pt-32 lg:px-28 xl:mx-auto xl:max-w-[80rem] xl:px-[8.5rem]">
+          <div className="rounded bg-[#1B181B] p-8 lg:p-10">
+            <div className="text-[1.56rem] font-semibold uppercase text-[#CFC9CF] sm:text-[2.44rem]">
+              an error occurred
+            </div>
 
-          <div className="pt-4 text-[0.8rem] text-[#CFC9CF] sm:text-[1rem]">
-            An error occurred while carrying out the operation. Click below to try again.
-          </div>
+            <div className="pt-4 text-[0.8rem] text-[#CFC9CF] sm:text-[1rem]">
+              An error occurred while carrying out the operation. Click below to
+              go back to the home page.
+            </div>
 
-          <div className="pt-6 sm:pt-10">
-            <button
-              className="inline-block rounded border border-[#F50057] bg-[#F50057] px-4 py-2 text-center text-[0.8rem] text-white sm:text-[1rem]"
-              onClick={() => {
-                reset();
-              }}
-            >
-              Try again
-            </button>
+            <div className="pt-6 sm:pt-10">
+              <Link
+                className="inline-block rounded border border-[#F50057] bg-[#F50057] px-4 py-2 text-center text-[0.8rem] text-white sm:text-[1rem]"
+                href='/'
+              >
+                Back to home
+              </Link>
+            </div>
           </div>
         </div>
       </div>
